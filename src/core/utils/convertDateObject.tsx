3a73a0820b4f0811d9bc242d44bab923831dc58f
Tanyawat Vittayapalotai <hamzaabamboo@gmail.com>
2020-03-26 12:17:59 +0700

    :bug: fix null value for converDateObject

--- src/core/utils/convertDateObject.tsx
+++ src/core/utils/convertDateObject.tsx
@@ -1,13 +1,17 @@
 import moment from 'moment';
 
 export const convertDateObject = (obj: any) => {
-  return {
-    guests: obj.guests || 1,
-    checkIn: new Date(obj.checkIn || ''),
-    checkOut: obj.checkOut
-      ? new Date(obj.checkOut || '')
-      : moment()
-          .add('day', 1)
-          .toDate(),
-  };
+  if (obj) {
+    return {
+      guests: obj.guests || 1,
+      checkIn: new Date(obj.checkIn || ''),
+      checkOut: obj.checkOut
+        ? new Date(obj.checkOut || '')
+        : moment()
+            .add('day', 1)
+            .toDate(),
+    };
+  } else {
+    return;
+  }
 };
