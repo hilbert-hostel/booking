58acd3aa64176b83333eb643c3cab3453e0f7e9e
Tanyawat Vittayapalotai <hamzaabamboo@gmail.com>
2020-04-24 01:08:00 +0700

    :bug: fix api url

--- src/core/repository/api/backend.tsx
+++ src/core/repository/api/backend.tsx
@@ -15,7 +15,7 @@ import { User } from '../../models/user';
 
 export let client: AxiosClient;
 
-if (!!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
   client = new AxiosClient('/');
 } else {
   client = new AxiosClient('https://hilbert.himkwtn.me/');
